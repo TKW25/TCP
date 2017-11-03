31e50781ff2e8b95c5c24106c714cc67eb524f34
TKW25 <tkw10@netlab-11.cs.pitt.edu>
2017-11-02 23:25:41 -0400

    IP Packet boilerplate complete

--- tcp_module.cc
+++ tcp_module.cc
@@ -2,7 +2,7 @@
 // stub that does nothing but integrate into the stack
 
 // For project parts A and B, an appropriate binary will be 
-// copied over as part of the build processd
+// copied over as part of the build process
 
 
 
@@ -25,10 +25,35 @@
 
 using namespace std;
 
+enum eState {
+    CLOSED = 0,
+    LISTEN = 1,
+    SYN_RCVD = 2,
+    SYN_SENT = 3,
+    ESTABLISHED = 4,
+    SEND_DATA = 5,
+    CLOSE_WAIT = 6,
+    FIN_WAIT1 = 7,
+    CLOSING = 8,
+    LAST_ACK = 9,
+    FIN_WAIT2 = 10,
+    TIME_WAIT = 11
+};
+
 struct TCPState {
-    // need to write this
+    eState state; //Current state, defined by enum eState
+    unsigned int last_acked; 
+    unsigned int last_sent; 
+    Buffer SendBuffer;
+    Buffer RecvBuffer;
+    unsigned short rwnd;
+    unsigned int last_recv;
+    unsigned int win_size;
+    time_t timer;
     std::ostream & Print(std::ostream &os) const { 
-	os << "TCPState()" ; 
+	os << "TCPState()\n" ; 
+	os << state << endl << last_acked << endl << last_sent << endl;
+	os << rwnd << endl << last_recv << endl << win_size << endl << timer << endl;
 	return os;
     }
 };
@@ -72,14 +97,82 @@ int main(int argc, char * argv[]) {
 
     MinetEvent event;
     double timeout = 1;
+    //Debugging bools, set to true to test if we handle errors correctly
+    bool corrupt = false;
+    bool reorder = false;
+    bool drop = false;
 
     while (MinetGetNextEvent(event, timeout) == 0) {
-
 	if ((event.eventtype == MinetEvent::Dataflow) && 
 	    (event.direction == MinetEvent::IN)) {
 	
 	    if (event.handle == mux) {
-		// ip packet has arrived!
+		// ip packet has arrived
+		cerr << "IP Packet received\n";
+		Packet p;
+		bool checksumok;
+		MinetReceive(mux, p);
+		unsigned short len = TCPHeader::EstimateTCPHeaderLength(p);
+		p.ExtractHeaderFromPayload<TCPHeader>(len);
+		TCPHeader tcph = p.FindHeader(Headers::TCPHeader);
+		IPHeader iph = p.FindHeader(Headers::IPHeader);
+		cerr << iph << "~~~~~~~~~~~~~~\n";
+		cerr << tcph << "~~~~~~~~~~~~~~~\n";
+		checksumok = tcph.IsCorrectChecksum(p);
+		Connection c;
+		iph.GetDestIP(c.src);
+		iph.GetSourceIP(c.dest);
+		iph.GetProtocol(c.protocol);
+		tcph.GetDestPort(c.srcport);
+		tcph.GetSourcePort(c.destport);
+		//End copied UDP boilerplate
+
+		//Get tcp and ip header information
+		unsigned char flags; //URG, ACK, PSH, RST, SYN, FIN
+		tcph.GetFlags(flags);
+		unsigned int ack;
+		tcph.GetAckNum(ack);
+		unsigned int seq;
+		tcph.GetSeqNum(seq);
+		unsigned short ws;
+		tcph.GetWinSize(ws);
+		unsigned char thlen;
+		tcph.GetHeaderLen(thlen);
+		unsigned short clen;
+		iph.GetTotalLength(clen);
+		unsigned char ihlen;
+		iph.GetHeaderLength(ihlen);
+		//We need to get the contents length which is the total length - headers length
+		clen -= (thlen * 4) - (ihlen * 4);
+		//Get packet content
+		Buffer content = p.GetPayload().ExtractFront(clen);
+		//Get Connection's State
+		ConnectionList<TCPState>::iterator cs = clist.FindMatching(c);
+		//All relevant TCP information collected
+
+		//Error check and handle Packet
+		if(checksumok){
+		    //Handle packet
+		    switch(cs->state.state){ 
+			case CLOSED: break; //Default case do nothing not listening
+			case LISTEN: break; //Passive open, send SYN,ACK
+			case SYN_RCVD: break; //Waiting to receive ACK of SYN
+			case SYN_SENT: break; //Waiting to receive SYN,ACK or SYN
+			case ESTABLISHED: break; //Handshake complete, receive DATA or FIN
+			case SEND_DATA: break;  //Do nothing, in process of sending data. This state might not be necessary
+			case CLOSE_WAIT: break; //Do nothing, waiting for socket to send down CLOSE
+			case FIN_WAIT1: break; //Sent FIN waiting for ACK or FIN
+			case CLOSING: break; //Received FIN while waiting for ACK of FIN
+			case LAST_ACK: break; //Received FIN, sent ACK and FIN, now waiting for final ACK
+			case FIN_WAIT2: break; //Waiting for FIN after sending a FIN and receiving an ACK
+			case TIME_WAIT: break; //Received FIN sent  ACK, wait for timeout (2MSL) in case ACK gets lost
+		    }
+		}
+		else{
+		    //Packet corrupted print error monitor and do nothing
+		    cerr << "ERROR! Corrupted Packet\n";
+		    MinetSendToMonitor(MinetMonitoringEvent("ERROR! Corrupted Packet\n"));
+		}
 	    }
 
 	    if (event.handle == sock) {
