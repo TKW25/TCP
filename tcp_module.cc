5a3f569a56aaa2ad46f4cd3681d9d35fb46050df
TKW25 <tkw10@netlab-11.cs.pitt.edu>
2017-11-03 23:44:56 -0400

    Added packet construction function and some IP packet stuff

--- tcp_module.cc
+++ tcp_module.cc
@@ -22,6 +22,8 @@
 #include <iostream>
 
 #include "Minet.h"
+#include "tcp.h"
+#include "ip.h"
 
 using namespace std;
 
@@ -29,8 +31,8 @@ enum eState {
     CLOSED = 0,
     LISTEN = 1,
     SYN_RCVD = 2,
-    SYN_SENT = 3,
-    ESTABLISHED = 4,
+    ESTABLISHED = 3,
+    SYN_SENT = 4,
     SEND_DATA = 5,
     CLOSE_WAIT = 6,
     FIN_WAIT1 = 7,
@@ -40,14 +42,24 @@ enum eState {
     TIME_WAIT = 11
 };
 
+enum HeaderType {
+    SYN = 0,
+    ACK = 1, 
+    SYNACK = 3, 
+    FIN = 4,
+    FINACK = 5,
+    RESET = 6
+};
+
 struct TCPState {
     eState state; //Current state, defined by enum eState
+    //Following two unsigned ints use our sequence number
     unsigned int last_acked; 
-    unsigned int last_sent; 
+    unsigned int last_sent;  
     Buffer SendBuffer;
     Buffer RecvBuffer;
     unsigned short rwnd;
-    unsigned int last_recv;
+    unsigned int last_recv; //Last received sequence number
     unsigned int win_size;
     time_t timer;
     std::ostream & Print(std::ostream &os) const { 
@@ -57,6 +69,9 @@ struct TCPState {
 	return os;
     }
 };
+
+void MakeOutputPacket(Packet &p, ConnectionList<TCPState>::iterator cs, size_t size, bool t, HeaderType h);
+void WrapMinetSend(const MinetHandle &mux, Packet p, bool corrupt, bool reorder, bool drop);
 
 
 int main(int argc, char * argv[]) {
@@ -98,9 +113,9 @@ int main(int argc, char * argv[]) {
     MinetEvent event;
     double timeout = 1;
     //Debugging bools, set to true to test if we handle errors correctly
-    bool corrupt = false;
-    bool reorder = false;
-    bool drop = false;
+//    bool corrupt = false;
+//    bool reorder = false;
+//    bool drop = false;
 
     while (MinetGetNextEvent(event, timeout) == 0) {
 	if ((event.eventtype == MinetEvent::Dataflow) && 
@@ -155,10 +170,85 @@ int main(int argc, char * argv[]) {
 		    //Handle packet
 		    switch(cs->state.state){ 
 			case CLOSED: break; //Default case do nothing not listening
-			case LISTEN: break; //Passive open, send SYN,ACK
-			case SYN_RCVD: break; //Waiting to receive ACK of SYN
+			case LISTEN: //passive open, send SYN,ACK
+			    //We only want to do something in this state if we receive a SYN
+			    if(IS_SYN(flags)){
+				cerr << "SYN Received, responding with SYN,ACK...\n";
+				//In this state our connection is relatively incomplete so we need to update it
+				cs->state.state = SYN_RCVD;
+				cs->state.last_acked = cs->state.last_sent; //Set last_acked to our starting sequence number 
+				cs->state.last_recv = seq;
+				cs->bTmrActive = true;
+				cs->timeout = Time() + 21; //I'm not certain if it matters what we add as long as we add soemthing
+				cs->connection = c;
+				//Our connection's state should be all setup correctly now
+				//Make and send packet
+				Packet out; MakeOutputPacket(out, cs, 0, false, SYNACK);
+				cerr << out << endl;
+				MinetSend(mux, out);
+				cs->state.last_sent += 1; //Update our sequence number
+			    }	
+			    break;
+			case SYN_RCVD: //Waiting to receive ACK of SYN
+			    if(IS_ACK(flags)){
+				cs->state.state = ESTABLISHED;
+				cs->state.last_recv = seq;
+				cs->state.last_acked = ack;
+				cs->state.rwnd = ws;
+				cs->bTmrActive = false; //Currently no unack'd packets
+				//Send signal up to the socket
+				SockRequestResponse write(WRITE, cs->connection, content, 0, EOK);
+				MinetSend(sock, write);
+				//Socket should eventually respond down to us with a STATUS
+			    }
+			    break; 
+			case ESTABLISHED: //Handshake complete, receive data or FIN
+			    if(IS_FIN(flags)){
+				//Remote partner wishes to close connection
+				cs->state.state = CLOSE_WAIT;
+				cs->state.last_recv = seq;
+				cs->state.rwnd = ws;
+				cs->bTmrActive = true;
+				cs->timeout = Time() + 21;
+				Packet out; MakeOutputPacket(out, cs, 0, false, ACK);
+				MinetSend(mux, out);
+				cs->state.last_sent += 1;
+				//We need to inform the application remote partner has initiated close
+				SockRequestResponse write(CLOSE, cs->connection, content, 0, EOK);
+				MinetSend(sock, write);
+			    }
+			    if(IS_ACK(flags)){
+				//We might have received a FIN,ACK
+				//Also could have simply received an ACK for data we've sent
+				
+				//We need to check if we've received a duplicate ack
+				if(ack > cs->state.last_acked){
+				   //ack is new, note we might have a cumulative ack
+				   int cum = ack - cs->state.last_acked;
+				   cs->state.SendBuffer.Erase(0, cum); //erase all acked packets from buffer
+				   if(cs->state.SendBuffer.GetSize() == 0){
+					//Nothing in send buffer, no need to timeout
+					cs->bTmrActive = false;
+				   }
+				}
+				else{
+				   //Congestion control stuff would go here if we decide to implement it
+				}
+			    }
+			    if(clen > 0){
+				//We've received data to send up to the application
+				cs->state.last_recv = seq + content.GetSize();
+				cs->state.rwnd = ws;
+				cs->state.RecvBuffer.AddBack(content);
+				SockRequestResponse write(WRITE, cs->connection, cs->state.RecvBuffer, cs->state.RecvBuffer.GetSize(), EOK);
+				//We'll clear RecvBuffer once socket sends us down a status telling us it's read it
+				//But regardless we ACK
+				Packet out; MakeOutputPacket(out, cs, 0, false, ACK);
+				MinetSend(mux, out);
+				cs->state.last_sent += 1;
+			    }
+			    break;
 			case SYN_SENT: break; //Waiting to receive SYN,ACK or SYN
-			case ESTABLISHED: break; //Handshake complete, receive DATA or FIN
 			case SEND_DATA: break;  //Do nothing, in process of sending data. This state might not be necessary
 			case CLOSE_WAIT: break; //Do nothing, waiting for socket to send down CLOSE
 			case FIN_WAIT1: break; //Sent FIN waiting for ACK or FIN
@@ -189,4 +279,63 @@ int main(int argc, char * argv[]) {
     MinetDeinit();
 
     return 0;
+}
+
+void MakeOutputPacket(Packet &p, ConnectionList<TCPState>::iterator cs, size_t size, bool t, HeaderType h){
+    //We'll be doing this alot so it's more convenient to have this in a function
+    //Even if it makes the code a little more complex here
+    IPHeader iph;
+    TCPHeader tcph;
+    size += IP_HEADER_BASE_LENGTH + TCP_HEADER_BASE_LENGTH;
+    //Build IP header
+    iph.SetProtocol(IP_PROTO_TCP);
+    iph.SetSourceIP(cs->connection.src);
+    iph.SetDestIP(cs->connection.dest);
+    iph.SetTotalLength(size);
+    cerr << iph << endl;
+    //Push it on packet
+    p.PushFrontHeader(iph);
+    //Build TCP Header
+    tcph.SetHeaderLen(5, p);
+    tcph.SetSourcePort(cs->connection.srcport, p);
+    tcph.SetDestPort(cs->connection.destport, p);
+    tcph.SetAckNum(cs->state.last_recv + 1, p);
+    tcph.SetWinSize(cs->state.win_size, p);
+    tcph.SetUrgentPtr(0, p);
+    //Check if we're retransmitting
+    if(t){
+	//Since ack is the next expected packet
+	//we want to send the last ack number received
+	tcph.SetSeqNum(cs->state.last_acked, p); 
+    }
+    else{
+	//Otherwise just set it to last_sent
+	//Assumes we've properly updated last_sent before calling make packet
+	tcph.SetSeqNum(cs->state.last_sent, p);
+    }
+    //Get appropriate flags
+    unsigned char flags;
+    switch(h){
+	case SYN: SET_SYN(flags); break;
+	case ACK: SET_ACK(flags); break;
+	case SYNACK: SET_SYN(flags); SET_ACK(flags); break;
+	case FIN: SET_FIN(flags); break;
+	case FINACK: SET_FIN(flags); SET_ACK(flags); break;
+	case RESET: SET_RST(flags); break;
+	default: cerr << "Something is broke\n"; break;
+    }
+    tcph.SetFlags(flags, p);
+    tcph.RecomputeChecksum(p);
+    //Finished building TCP header
+    cerr << tcph << endl;
+    if(!tcph.IsCorrectChecksum(p)){
+	cerr << "Checksum failed in building packet, something is seriously wrong\n";
+    }
+    //Put it in the packet
+    p.PushBackHeader(tcph);
+} 
+
+void WrapMinetSend(const MinetHandle &mux, Packet p, bool corrupt, bool reorder, bool drop){
+    //This should be used for error testing but we're kinda short on time so probably won't be
+    MinetSend(mux, p);
 }
